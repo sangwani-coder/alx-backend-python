48ac7c010d787424f3e134e9b0f20368c898738c
zyambo <mightypz2@gmail.com>
2022-06-27 09:34:37 +0000

    await subroutine

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,4 +8,5 @@ async def wait_random(max_delay: int = 10) -> float:
     """ waits for a random delya between 0 and max_delay
         seconds and ventually returns it."""
     delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
