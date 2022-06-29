7b355c43650236316b0d15ff4bc821a900efa82d
zyambo <mightypz2@gmail.com>
2022-06-29 21:25:44 +0000

    document functions

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-""" Add function with type annotations """
+""" Defines a type annotated function add """
 
 
 def add(a: float, b: float) -> float:
+    """
+        a type-annotated function that takes
+        param: (float a and float b)
+            and
+        returns: their sum as a float.
+    """
     return (a + b)
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-""" Concat strings"""
+"""Defines a type annotated function concat"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """a type-annotated function that takes a
+        params: (str1 and str2)
+            and
+        returns: a concatenated string
+    """
     return(str1 + str2)
