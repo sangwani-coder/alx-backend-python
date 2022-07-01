4cb45b225ba593be4115ce7d4088d5eaba2696d7
zyambo <mightypz2@gmail.com>
2022-07-02 01:10:59 +0200

    change import style

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ measure runtime """
 import asyncio
-from timeit import default_timer
+import timeit
 import random
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -11,12 +11,12 @@ async def measure_runtime() -> float:
     """ executes comprehesion coroutine 4 times in parallel
         measure and return the total runtime
     """
-    start = default_timer()
+    start = timeit.default_timer()
     await asyncio.gather(
             async_comprehension(),
             async_comprehension(),
             async_comprehension(),
             async_comprehension()
             )
-    end = default_timer()
+    end = timeit.default_timer()
     return end - start
