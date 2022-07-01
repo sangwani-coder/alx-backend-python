73218aee51b1f3bc162c5b4d99d3e4ad692ab9ea
zyambo <mightypz2@gmail.com>
2022-07-02 00:53:30 +0200

    complete task 2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" measure runtime """
+import asyncio
+import time
+import random
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ executes comprehesion coroutine 4 times in parallel
+        measure and return the total runtime
+    """
+    start = time.time()
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+            )
+    end = time.time()
+    return end - start
+
