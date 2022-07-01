b67804f368c5ba4027ad97419a89bbb56f41b5c9
zyambo <mightypz2@gmail.com>
2022-07-02 00:25:28 +0200

    [FIX] change random.uniform() to random.random()

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,11 +7,10 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, float, float]:
     """
-        a coroutine that takes no arguments. loops 10 times,
-        each time asynchronously wait 1 second,
+        loops 10 times and asynchronously wait 1 second,
         then yield a random number between 0 and 10.
     """
 
     for i in range(10):
+        yield random.random()
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
